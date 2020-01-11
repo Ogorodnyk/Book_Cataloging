4aea3a4ca8b501dda054e964a4f84a67095e7d85
Maksym Ogorodnyk <maksym.ogorodnyk1@gmail.com>
2020-01-11 16:19:19 +0100

    Poprawione

---
+++ books-workshop/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/ogorodnyk/Desktop/KAT_PYT_W_05_REST_Server/books-workshop/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
